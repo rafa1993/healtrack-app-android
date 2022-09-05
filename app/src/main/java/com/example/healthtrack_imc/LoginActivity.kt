085b6a8be5b77ece1a0931d69c3721a99c0a5eeb
rafaela hristidis morais <r.hristidis.morais@accenture.com>
2022-09-05 19:41:48 -0300

    login update

--- app/src/main/java/com/example/healthtrack_imc/LoginActivity.kt
+++ app/src/main/java/com/example/healthtrack_imc/LoginActivity.kt
@@ -1,3 +1,46 @@
 package com.example.healthtrack_imc
 
-import android.content.Intentimport android.os.Bundleimport android.view.Viewimport android.widget.Toastimport androidx.appcompat.app.AppCompatActivityimport androidx.lifecycle.Observerimport androidx.lifecycle.ViewModelProviderimport com.example.healthtrack_imc.databinding.LoginActivityBindingclass LoginActivity : AppCompatActivity(), View.OnClickListener {        private lateinit var binding: LoginActivityBinding        private lateinit var viewModel: LoginViewModel        override fun onCreate(savedInstanceState: Bundle?) {            super.onCreate(savedInstanceState)            binding = LoginActivityBinding.inflate(layoutInflater)            setContentView(binding.root)            viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)            binding.buttonLogin.setOnClickListener(this)            createObservers()        }        override fun onClick(v: View?) {          val name = binding.editName.text.toString()          viewModel.doLogin(name)            binding.buttonLogin.setOnClickListener{               val back = Intent(this, IMCActivity::class.java)                startActivity(back)            }        }        private fun createObservers() {            viewModel.welcome().observe(this, Observer {                binding.textWelcome            })            viewModel.login().observe(this, Observer {                Toast.makeText(this, it, Toast.LENGTH_SHORT).show()            })        }    }
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.healthtrack_imc.databinding.LoginActivityBinding
+
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
+
+        private lateinit var binding: LoginActivityBinding
+        private lateinit var viewModel: LoginViewModel
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            binding = LoginActivityBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+
+            viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+
+            binding.buttonLogin.setOnClickListener(this)
+
+            createObservers()
+        }
+
+        override fun onClick(v: View?) {
+          val name = binding.editName.text.toString()
+          viewModel.doLogin(name)
+            binding.buttonLogin.setOnClickListener{
+               val back = Intent(this, IMCActivity::class.java)
+                startActivity(back)
+            }
+        }
+
+        private fun createObservers() {
+            viewModel.welcome().observe(this, Observer {
+                binding.textWelcome
+            })
+            viewModel.login().observe(this, Observer {
+                Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+            })
+        }
+    }
